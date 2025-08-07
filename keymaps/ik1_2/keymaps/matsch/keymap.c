bcf1d33d42b349a365f1a61c92eee19b2c691270
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-08-07 16:06:57 +0200

    add double point to NUM layer

--- keymaps/ik1_2/keymaps/matsch/keymap.c
+++ keymaps/ik1_2/keymaps/matsch/keymap.c
@@ -51,8 +51,8 @@ KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,               
 [LNUM] = LAYOUT_split_4x6_3(
 _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,
 _______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_GRV,                             _______,    KC_7,       KC_8,       KC_9,       _______,    _______,
-_______,    KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_BSLS,                            _______,    KC_4,       KC_5,       KC_6,       _______,    _______,
-_______,    KC_PLUS,    KC_MINS,    KC_ASTR,    KC_SLSH,    KC_EQL,                             _______,    KC_1,       KC_2,       KC_3,       _______,    _______,
+_______,    KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_BSLS,                            _______,    KC_4,       KC_5,       KC_6,       KC_SCLN,    _______,
+_______,    KC_PLUS,    KC_MINS,    KC_ASTR,    KC_SLSH,    KC_EQL,                             _______,    KC_1,       KC_2,       KC_3,       KC_SLSH,    _______,
                                                 KC_LALT,    KC_LCTL,     KC_SPC,    KC_ENT,     KC_MINS,    KC_0
 ),
 
