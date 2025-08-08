eb84ca8e0a30f3f556c580c9ae2c8d41b7cc63e3
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-08-08 08:46:36 +0200

    add Enter to left keyboard additionaly to ALT key

--- keymaps/ik1_2/keymaps/matsch/keymap.c
+++ keymaps/ik1_2/keymaps/matsch/keymap.c
@@ -13,6 +13,7 @@
 
 #define NAV MO(LNAV)
 #define BSSFT MT(MOD_RSFT,KC_BSPC)
+#define ENTALT MT(KC_LALT,KC_ENT)
 #define FN MO(LFN)
 #define BASE DF(LBASE)
 
@@ -45,7 +46,7 @@ _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,               
 KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
 KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
 KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                               KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RGUI,
-                                                KC_LALT,    KC_LCTL,     SPCNUM,    ENTNAV,     MINFN,      BSSFT
+                                                ENTALT,     KC_LCTL,     SPCNUM,    ENTNAV,     MINFN,      BSSFT
 ),
 
 [LNUM] = LAYOUT_split_4x6_3(
@@ -53,7 +54,7 @@ _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,              
 _______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_GRV,                             _______,    KC_7,       KC_8,       KC_9,       _______,    _______,
 _______,    KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_BSLS,                            _______,    KC_4,       KC_5,       KC_6,       KC_SCLN,    _______,
 _______,    KC_PLUS,    KC_MINS,    KC_ASTR,    KC_SLSH,    KC_EQL,                             _______,    KC_1,       KC_2,       KC_3,       KC_SLSH,    _______,
-                                                KC_LALT,    KC_LCTL,     KC_SPC,    KC_ENT,     KC_MINS,    KC_0
+                                                ENTALT,     KC_LCTL,     KC_SPC,    KC_ENT,     KC_MINS,    KC_0
 ),
 
 [LNAV] = LAYOUT_split_4x6_3(
@@ -61,7 +62,7 @@ _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,              
 _______,    _______,    _______,    KC_LCBR,    KC_RCBR,    KC_TILD,                            KC_PGUP,    KC_HOME,    KC_UP,      KC_END,     KC_PSCR,    KC_BSPC,
 _______,    _______,    _______,    KC_LBRC,    KC_RBRC,    KC_PIPE,                            KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   _______,    _______,
 _______,    _______,    _______,    KC_LPRN,    KC_RPRN,    _______,                            KC_INS,     KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,    _______,
-                                                KC_LALT,    KC_LCTL,     KC_SPC,    KC_ENT,     KC_MINS,    BSSFT
+                                                ENTALT,     KC_LCTL,     KC_SPC,    KC_ENT,     KC_MINS,    BSSFT
 ),
 
 [LFN] = LAYOUT_split_4x6_3(
