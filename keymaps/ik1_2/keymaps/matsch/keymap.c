8f25f736287cbdd35511d8b8cf1e235b701b61b4
Matthias Schaefer <matsch1@gmx.net>
2025-10-01 20:03:10 +0200

    Switch Layout to English US intl. with AltGr Dead keys on Linux and got the behaviour I want

--- keymaps/ik1_2/keymaps/matsch/keymap.c
+++ keymaps/ik1_2/keymaps/matsch/keymap.c
@@ -17,28 +17,21 @@
 #define FN MO(LFN)
 #define BASE DF(LBASE)
 
-enum unicode_names {
-    A_UMLAUT,
-    A_UMLAUT_CAP,
-    O_UMLAUT,
-    O_UMLAUT_CAP,
-    U_UMLAUT,
-    U_UMLAUT_CAP,
-    ESZETT,
-    ESZETT_CAP
+enum custom_keycodes {
+  VIM_YANK = SAFE_RANGE,
 };
 
-const uint32_t PROGMEM unicode_map[] = {
-    [A_UMLAUT]     = 0x00E4, // ä
-    [A_UMLAUT_CAP] = 0x00C4, // Ä
-    [O_UMLAUT]     = 0x00F6, // ö
-    [O_UMLAUT_CAP] = 0x00D6, // Ö
-    [U_UMLAUT]     = 0x00FC, // ü
-    [U_UMLAUT_CAP] = 0x00DC, // Ü
-    [ESZETT]       = 0x00DF, // ß
-    [ESZETT_CAP]   = 0x1E9E  // ẞ
-};
-
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case VIM_YANK:
+      if (record->event.pressed) {
+        // when key is pressed
+        SEND_STRING("\"+y");
+    }
+    break;
+  }
+  return true;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LBASE] = LAYOUT_split_4x6_3(
@@ -66,10 +59,10 @@ _______,    _______,    _______,    KC_LPRN,    KC_RPRN,    _______,            
 ),
 
 [LFN] = LAYOUT_split_4x6_3(
-_______,    KC_F1,                      KC_F2,                  KC_F3,      KC_F4,       KC_F5,                             KC_F6,      KC_F7,                      KC_F8,      KC_F9,                      KC_F10,     _______,
-_______,    KC_F11,                     KC_F12,                 _______,    _______,     _______,                           _______,    UP(U_UMLAUT, U_UMLAUT_CAP), _______,    UP(O_UMLAUT, O_UMLAUT_CAP), _______,    _______,
-_______,    UP(A_UMLAUT, A_UMLAUT_CAP), UP(ESZETT, ESZETT_CAP), _______,    _______,     _______,                           _______,    _______,                    _______,    _______,                    _______,    _______,
-_______,    LSFT(KC_QUOT),              KC_PLUS,                KC_Y,       _______,     _______,                           UC_NEXT,    _______,                    _______,    _______,                    _______,    _______,
+_______,    KC_F1,           KC_F2,           KC_F3,      KC_F4,       KC_F5,                             KC_F6,      KC_F7,       KC_F8,      KC_F9,      KC_F10,     _______,
+_______,    KC_F11,          KC_F12,          _______,    _______,     _______,                           _______,    RALT(KC_Y),  _______,    RALT(KC_P), _______,    _______,
+_______,    RALT(KC_Q),      RALT(KC_S),      _______,    _______,     _______,                           _______,    _______,     _______,    _______,    _______,    _______,
+KC_LSFT,    VIM_YANK,        LSFT(KC_QUOT),   KC_PLUS,    KC_Y,        _______,                           UC_NEXT,    _______,     _______,    _______,    _______,    _______,
                                                                             KC_LALT,    KC_LCTL,     KC_SPC,    KC_ENT,     KC_MINS,      BSSFT
 ),
 
